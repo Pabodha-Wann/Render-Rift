05bb27bc28e1baf4099d35f238effacbb8846b5b
Pabodha Wanniarachchi <pabodhawanniarachchi@gmail.com>
2025-09-01 00:56:54 +0530

    commit

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useLocation, useNavigate } from "react-router-dom"
 import "../assets/css/Header.css"
-import "../assets/css/search.css"
+import "../assets/css/Search.css"
 import GameCard from "../components/GameCard"
 import { fetchsearch } from '../RWGapi';
 import { IoIosSearch } from "react-icons/io";
